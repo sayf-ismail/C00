457c45be59d2127473db35f40920f612303c41b2
Sayf Tun-ismail <stun-ism@u91z04s02.42kl.edu.my>
2021-02-20 13:37:29 +0800

    fixed ex00 according to norm

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -13,15 +13,13 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void ft_putchar(char c);
-
-int main(void)
-{
-	ft_putchar('c');
-	return(0);
-}
-
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	ft_putchar('c');
+	return (0);
 }
