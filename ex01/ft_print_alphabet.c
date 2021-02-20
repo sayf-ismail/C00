30598aba03e3805c505ad16a4f5290ffa174c637
Sayf Tun-ismail <stun-ism@u91z04s02.42kl.edu.my>
2021-02-20 14:13:55 +0800

    completed ex00 and ex01. Passed Norm

--- ex01/ft_print_alphabet.c
+++ ex01/ft_print_alphabet.c
@@ -1,24 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stun-ism <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/20 12:58:50 by stun-ism          #+#    #+#             */
-/*   Updated: 2021/02/20 12:59:33 by stun-ism         ###   ########.fr       */
+/*   Created: 2021/02/20 14:06:10 by stun-ism          #+#    #+#             */
+/*   Updated: 2021/02/20 14:12:41 by stun-ism         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_print_alphabet(void)
 {
-	write(1, &c, 1);
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		write(1, &c, 1);
+		c++;
+	}
 }
 
 int		main(void)
 {
-	ft_putchar('c');
+	ft_print_alphabet();
 	return (0);
 }
