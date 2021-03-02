a6d6530d2f884c7806116ba0197b2b083eb9e563
Sayf Tun-ismail <stun-ism@u90z02s03.42kl.edu.my>
2021-03-02 14:11:45 +0800

    fixed repo before evaluations

--- ex05/main.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-void ft_print_comb(void);
-
-int		main(void) 
-{
-	ft_print_comb();
-	return (0);
-}
--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -1,24 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   v4_ft_putnbr.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stun-ism <stun-ism@student.42kl.edu>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/26 13:47:28 by stun-ism          #+#    #+#             */
-/*   Updated: 2021/02/26 13:53:39 by stun-ism         ###   ########.fr       */
+/*   Updated: 2021/03/02 14:05:19 by stun-ism         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putchar(char l)
+{
+	write(1, &l, 1);
+}
+
 void	ft_putnbr(int nb)
 {
-	char tens;
-	char units;
+	char c;
 
-	tens = '0' + nb / 10;
-	units = '0' + nb % 10;
-	write(1, &tens, 1);
-	write(1, &units, 1);
+	if (nb < 0)
+	{
+		c = '-';
+		ft_putchar(c);
+		nb *= -1;
+	}
+	if (nb)
+	{
+		ft_putnbr(nb / 10);
+		c = '0' + (nb % 10);
+		ft_putchar(c);
+	}
+	else
+		return ;
 }
